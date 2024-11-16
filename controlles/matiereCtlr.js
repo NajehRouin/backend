3eff67736cbfd7b8dd73ae9ae72595e44dc7a604
NajehRouin <rouin.najeh71@gmail.com>
2024-11-16 23:09:36 +0100

    delet composant of matiere

--- controlles/matiereCtlr.js
+++ controlles/matiereCtlr.js
@@ -1,121 +1,117 @@
-let matiere=require('../models/matiereModel')
+let matiere = require("../models/matiereModel");
+let composant = require("../models/componentModel");
+let mongoose = require("mongoose");
 
+let matiereCtrl = {
+  AjouterMatiere: async (req, res) => {
+    try {
+      let { nom, prix } = req.body;
 
-let matiereCtrl={
+      let findMatiere = await matiere.findOne({ nom });
 
-    AjouterMatiere:async(req,res)=>{
-        try {
-            let {nom,prix}=req.body
+      if (findMatiere)
+        return res.status(302).json({ msg: "matiére déja existe" });
 
-            let findMatiere=await matiere.findOne({nom})
+      if (!nom) return res.status(302).json({ msg: "nom  Obligatoire" });
+      if (!prix) return res.status(302).json({ msg: "prix  Obligatoire" });
 
-            if(findMatiere)return res.status(302).json({ msg: "matiére déja existe" });
+      let newMatier = new matiere({ nom, prix });
 
-            if(!nom)  return res.status(302).json({ msg: "nom  Obligatoire" });
-            if(!prix)  return res.status(302).json({ msg: "prix  Obligatoire" });
+      await newMatier.save();
 
-            let newMatier=new matiere({nom,prix})
-
-            await newMatier.save()
-
-            res.status(201).json({
-                data : newMatier,
-                success : true,
-                error : false,
-                message : "matiére créé avec succès !"
-            })
-            
-        } catch (err) {
-            res.json({
-                msg : err.message || err  ,
-                error : true,
-                success : false,
-            })
-        }
-    },
-    ModifierMatier:async(req,res)=>{
-        try {
-            const {matierId}=req.body
-            const payload = {
-                ...req.body,
-            }
-
-            
-            const updateMatier = await matiere.findByIdAndUpdate(matierId,payload)
-            res.json({
-                data : updateMatier,
-                message : "matiére Updated",
-                success : true,
-                error : false
-            })
-            
-        }  catch (err) {
-            res.json({
-                msg : err.message || err  ,
-                error : true,
-                success : false,
-            })
-        }
-    },
-
-    getMatierByID:async(req,res)=>{
-        try {
-            const {matierId}=req.body
-
-            let findMatier=await matiere.findById(matierId)
-            res.json({
-                data : findMatier,
-                success : true,
-                error : false
-            })
-            
-        } catch (err) {
-            res.json({
-                msg : err.message || err  ,
-                error : true,
-                success : false,
-            })
-        }
-    },
-
-    deleteMatier:async(req,res)=>{
-        try {
-            const {matierId}=req.body
-            let findMatierAndDelete=await matiere.findByIdAndDelete(matierId)
-            res.json({
-                data : findMatierAndDelete,
-                message : "matiére Deleted",
-                success : true,
-                error : false
-            })
-        }  catch (err) {
-            res.json({
-                msg : err.message || err  ,
-                error : true,
-                success : false,
-            })
-        }
-    },
-    getAllMatier:async(req,res)=>{
-        try {
-            let findMatiers=await matiere.find()
-            res.json({
-                data : findMatiers,
-                
-                success : true,
-                error : false
-            })
-            
-        } catch (err) {
-            res.json({
-                msg : err.message || err  ,
-                error : true,
-                success : false,
-            })
-        }
+      res.status(201).json({
+        data: newMatier,
+        success: true,
+        error: false,
+        message: "matiére créé avec succès !",
+      });
+    } catch (err) {
+      res.json({
+        msg: err.message || err,
+        error: true,
+        success: false,
+      });
     }
+  },
+  ModifierMatier: async (req, res) => {
+    try {
+      const { matierId } = req.body;
+      const payload = {
+        ...req.body,
+      };
 
-}
+      const updateMatier = await matiere.findByIdAndUpdate(matierId, payload);
+      res.json({
+        data: updateMatier,
+        message: "matiére Updated",
+        success: true,
+        error: false,
+      });
+    } catch (err) {
+      res.json({
+        msg: err.message || err,
+        error: true,
+        success: false,
+      });
+    }
+  },
 
+  getMatierByID: async (req, res) => {
+    try {
+      const { matierId } = req.body;
 
-module.exports=matiereCtrl
+      let findMatier = await matiere.findById(matierId);
+      res.json({
+        data: findMatier,
+        success: true,
+        error: false,
+      });
+    } catch (err) {
+      res.json({
+        msg: err.message || err,
+        error: true,
+        success: false,
+      });
+    }
+  },
+
+  deleteMatier: async (req, res) => {
+    try {
+      const { matierId } = req.body;
+
+      let findMatierAndDelete = await matiere.findByIdAndDelete(matierId);
+      await composant.deleteMany({ matiere: matierId });
+      res.json({
+        data: findMatierAndDelete,
+        message: "matiére Deleted",
+        success: true,
+        error: false,
+      });
+    } catch (err) {
+      res.json({
+        msg: err.message || err,
+        error: true,
+        success: false,
+      });
+    }
+  },
+  getAllMatier: async (req, res) => {
+    try {
+      let findMatiers = await matiere.find();
+      res.json({
+        data: findMatiers,
+
+        success: true,
+        error: false,
+      });
+    } catch (err) {
+      res.json({
+        msg: err.message || err,
+        error: true,
+        success: false,
+      });
+    }
+  },
+};
+
+module.exports = matiereCtrl;
--- routes/Matiere.Routes.js
+++ routes/Matiere.Routes.js
@@ -1,13 +1,33 @@
-let router=require('express').Router()
-let auth=require('../middleware/auth')
-let isAdmin=require('../middleware/authRole')
+let router = require("express").Router();
+let auth = require("../middleware/auth");
+let isAdmin = require("../middleware/authRole");
 
-let matiereCtrl=require('../controlles/matiereCtlr')
+let matiereCtrl = require("../controlles/matiereCtlr");
 
-router.get('/matiere',matiereCtrl.getAllMatier)
-router.post('/matiere',auth.auhAdmin,isAdmin.Role_Admin,matiereCtrl.AjouterMatiere)
-router.post('/matierId',auth.auhAdmin,isAdmin.Role_Admin,matiereCtrl.getMatierByID)
-router.put('/matiere',auth.auhAdmin,isAdmin.Role_Admin,matiereCtrl.ModifierMatier)
-router.delete('/matiere',auth.auhAdmin,isAdmin.Role_Admin,matiereCtrl.deleteMatier)
+router.get("/matiere", matiereCtrl.getAllMatier);
+router.post(
+  "/matiere",
+  auth.auhAdmin,
+  isAdmin.Role_Admin,
+  matiereCtrl.AjouterMatiere
+);
+router.post(
+  "/matierId",
+  auth.auhAdmin,
+  isAdmin.Role_Admin,
+  matiereCtrl.getMatierByID
+);
+router.put(
+  "/matiere",
+  auth.auhAdmin,
+  isAdmin.Role_Admin,
+  matiereCtrl.ModifierMatier
+);
+router.delete(
+  "/matiere",
+  //   auth.auhAdmin,
+  //   isAdmin.Role_Admin,
+  matiereCtrl.deleteMatier
+);
 
-module.exports=router
+module.exports = router;
